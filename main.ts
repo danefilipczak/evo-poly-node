89e600b3c6e7ec7bec04e90b267c02d3033a1a30
df069g <df069g@att.com>
2018-11-23 15:38:03 -0800

    [code] updating main

--- main.ts
+++ main.ts
@@ -2,6 +2,21 @@ import * as Population from './src/Population/Population'
 import * as Parse from './src/IO/Parse';
 const path = require('path');
 
+// if (process.platform === "win32") {
+//     require("readline").createInterface({
+//         input: process.stdin,
+//         output: process.stdout
+//     }).on("SIGINT", function () {
+//         process.emit("SIGINT");
+//     });
+// }
+
+process.on("SIGINT", function () {
+    //stop the evolution, export the best;
+    console.log('shittung down')
+    process.exit();
+});
+
 const simulate = (population: number[][][][]) => {
     population = Population.evolve(population);
 }
